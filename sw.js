d4078c2fc4cd71802b1cefc37b2220b00b073cc5
max umbra <betooo.neh@gmail.com>
2026-02-10 20:35:06 -0600

    Add report 2026-02-09-1017

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'signal-monitor-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
